7c77051b92ceb49d1f33132c0c99dd4a48d02b16
omar <omarspc@omarsPC.lan>
2024-01-30 15:05:55 -0500

    added more examples

---
+++ mergeStrings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        newWord = []
+        maxLen = max(len(word1),len(word2))
+
+        for i in range(maxLen):
+            if i < len(word1):
+                newWord.append(word1[i])
+
+            if i < len(word2):
+                newWord.append(word2[i])
+
+        return ''.join(newWord)
